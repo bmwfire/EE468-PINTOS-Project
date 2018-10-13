a8d47a4f449819a106cebdbb597c34669c50f20e
Charles Dickens <charlieadickens@gmail.com>
2018-10-13 13:06:07 -1000

    ;s

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -115,9 +115,9 @@ syscall_handler (struct intr_frame *f)
           f->eax = (uint32_t) sys_write((int) *(esp+1), (const void*) *(esp+2), (unsigned) *(esp+3));
         else{
           if(!is_valid_ptr((const void*)(*(esp+2)))){
-            printf("Write: %s \n", (char *)(esp + 2))
-            printf("write: %x \n" (esp + 2))
-            printf("write: %x \n" *(esp + 2))
+            printf("Write: %s \n", (char *)(esp + 2));
+            printf("write: %x \n" (esp + 2));
+            printf("write: %x \n" *(esp + 2));
             printf("WRITE: *(esp+2) invalid \n");
           }
           if(!is_valid_ptr((const void*)((*(esp+2)+*(esp+3)-1)))){
